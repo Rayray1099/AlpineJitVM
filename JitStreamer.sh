d09c3e2e68bce72e20d08a2309051fa6799707ed
Rayray1099 <95627112+Rayray1099@users.noreply.github.com>
2025-01-22 14:17:07 +0800

    Update JitStreamer.sh

--- JitStreamer.sh
+++ JitStreamer.sh
@@ -4,13 +4,6 @@
 echo "Welcome to JitStreamer!"
 echo "Starting the server. This might take a few seconds..."
 
-# Run tailscale up
-tailscale up
-
-# Get and display the Tailscale IPv4 address
-TAILSCALE_IP=$(tailscale ip -4)
-echo "This will be your server IP: $TAILSCALE_IP"
-
 # Run usbmuxd
 usbmuxd
 
